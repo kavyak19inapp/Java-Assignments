2dcf5afe0fde5b7eba5c498ee26a84b3b764d350
kavyak19inapp <41429072+kavyak19inapp@users.noreply.github.com>
2018-08-17 09:54:16 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,20 @@
+package com.inapp.operators.util;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a=2,b=3,c=40;
+		String x="hello";
+		String y="world";	
+	    System.out.println(a+b);
+	    System.out.println(a-b);
+	    System.out.println(a*b);
+	    System.out.println(a/b);
+	    System.out.println(a%b);
+	    System.out.println(x+y);
+	    System.out.println("a==b:"+(a==b));
+	    System.out.println("a>>3:"+(a>>3));
+	    System.out.println("Concatenation (x+y)= "+a+b);
+
+}
+}
